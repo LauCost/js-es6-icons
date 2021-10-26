e8c0026168da0b7d00a30c469c628b5f36f4d400
Laurentiu <lcostin40@gmail.com>
2021-10-26 23:05:53 +0200

    Aggiunto codice js per filtrare le icone

--- JS/milestone_1.js
+++ JS/milestone_1.js
@@ -98,49 +98,75 @@ const post = [
     }
 ];
 
-//Select the container in which it will print the content
-const container = document.querySelector(".row");
 
-//ForEach that will read the array and print the objects in DOM
-post.forEach(post => {
+function stampaIcone() {
 
+    //  ************ Milestone 3  ************
+    //Select the option of the user and add it to a const
+    const selettore = document.getElementById("type");
+    //  ************ /Milestone 3  ************
 
+    //Select the container in which it will print the content
+    const container = document.querySelector(".row");
 
+    container.innerHTML = "";
 
-    //  ************ Milestone 2  ************
-    //Empty array of colors
-    let color = [];
+    //ForEach that will read the array and print the objects in DOM
+    post.forEach(post => {
 
-    //An if that will check the type of icon and assign the color
-    if (post.type == "animal") {
-
-        color = "#0000FF"
-
-    } else if (post.type == "vegetable") {
-
-        color = "#FFA500"
-
-    } else if (post.type == "user") {
-
-        color = "#800080"
-
-    }
-    //  ************ /Milestone 2  ************
+        //  ************ Milestone 3  ************
+        if (selettore.value == post.type || selettore.value == "all") {
 
 
+            //  ************ Milestone 2  ************
+            //Empty array of colors
+            let color = [];
+
+            //An if that will check the type of icon and assign the color
+            if (post.type == "animal") {
+
+                color = "#0000FF"
+
+            } else if (post.type == "vegetable") {
+
+                color = "#FFA500"
+
+            } else if (post.type == "user") {
+
+                color = "#800080"
+
+            }
+            //  ************ /Milestone 2  ************
 
 
 
-    //Print the objects of array in DOM
-    const cardPost = `
-    
-        <div class="col">
-            <div class="post" type="${post.type}" >
-                <i class="${post.family} ${post.prefix}${post.name}" style="color: ${color};"></i>
-                <p>${post.name.toUpperCase()}</p>
-            </div>
-        </div>
-    `
 
-    container.innerHTML += cardPost
-});
+
+            //Print the objects of array in DOM
+            const cardPost = `
+            
+                <div class="col">
+                    <div class="post" type="${post.type}" >
+                        <i class="${post.family} ${post.prefix}${post.name}" style="color: ${color};"></i>
+                        <p>${post.name.toUpperCase()}</p>
+                    </div>
+                </div>
+            `
+
+            container.innerHTML += cardPost
+        }
+    });
+}
+
+
+
+//  ************ Milestone 3  ************
+//Calls the function
+stampaIcone()
+
+//Select the option of the user and add it to a const
+const selettore = document.getElementById("type");
+//It adds event listener to change the value for the function
+selettore.addEventListener(`change`, stampaIcone)
+//  ************ /Milestone 3  ************
+
